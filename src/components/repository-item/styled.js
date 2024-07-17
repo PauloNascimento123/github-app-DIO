16245cf4b4e8145c1b98d86c4d8d6e26ae7e70ce
PauloNascimento123 <paulo.nascimento.paulo@gmail.com>
2024-07-17 06:40:43 -0300

    Primeiro commit

---
+++ src/components/repository-item/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    margin: 16px 16px;
+    padding: 8px;
+    align-content: center;
+    width: 350px;
+    height: 120px;
+`;
+
+export const WrapperTitle = styled.h2`
+    font-size: 18px;
+    font-weight: bold;
+    margin: 8px 0;
+`;
+
+export const WrapperFullName = styled.h4`
+    font-size: 14px;
+    font-weight: bold;
+    margin: 8px 0;
+    color: #2d3748;
+`;
+
+export const WrapperLink = styled.a`
+    font-size: 16px;
+    font-weight: bold;
+    margin: 8px 0;
+    color: #3182ce;
+`;
